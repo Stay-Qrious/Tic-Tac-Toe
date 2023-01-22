725c06ff597e922456c7720fd59929b0b9ff5ff6
Stay-Qrious <103578108+Stay-Qrious@users.noreply.github.com>
2023-01-22 18:57:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+console.log("Welcome to tic tac toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X";
+let isgameover = false;
+music.play();
+
+// Function to change the Turn 
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
+
+}
+
+// Function to Check for a win
+const CheckWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            music.pause();
+            gameover.play();
+            
+        }
+    })
+
+}
+
+// Game Logic 
+// music.play();
+
+let boxes = document.getElementsByClassName("box")
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            CheckWin();
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+        }
+    })
+})
+
+// add onclick listner to reset button 
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"
+    isgameover = false
+    gameover.pause();
+    music.currentTime=0;
+    gameover.currentTime=0;
+    music.play();
+
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+})
+
+
+
+
+
+
